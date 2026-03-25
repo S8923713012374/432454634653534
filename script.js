1fef505c9e8476767a55c2415099bd1de286dde0
Green Ecology <ecologica.verde.backup1@gmail.com>
2026-03-25 19:12:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ const CONFIG = {
     
     sourceUrls: {
         'byxatab': 'https://byxatab.pages.dev/',
-        'dodi': 'https://40dcb8a5.dodi.pages.dev/',
-        'ecologica': 'https://d9a6c21a.ecologica2verde.pages.dev/',
-        'fitgirl': 'https://118cf879.ecofitgirl.pages.dev/',
+        'dodi': 'https://dodi.pages.dev/',
+        'ecologica': 'https://ecologica2verde.pages.dev/',
+        'fitgirl': 'https://ecofitgirl.pages.dev/',
         'gog': 'https://freepcgoggames.pages.dev/',
-        'onlinefix': 'https://394712e2.onlinefixme.pages.dev/',
+        'onlinefix': 'https://onlinefixme.pages.dev/',
         'insaneramzes': 'https://insaneramzes.pages.dev/'
     },
     
